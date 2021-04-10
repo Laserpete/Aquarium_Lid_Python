cbbbe2f6430ea73fb91127cf8cde8ff378a1afd7
LaserPete <pete.t.doyle@gmail.com>
2021-04-10 10:55:52 +0200

    Testing new test.py

--- examples/test.py
+++ examples/test.py
@@ -15,6 +15,19 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
+from sensor import HTU21D
+
+htu21d = HTU21D(1, 0x40)
+
+humid = htu21d.humidity()
+humid = round(humid.RH)
+humidity str(humid)
+
+temp = htu21d.temperature()
+C, F, K = temp
+C = round(C,2)
+temperature = str(C)
+
 try:
     logging.info("epd2in9 V2 Demo") 
     epd = epd2in9_V2.EPD()
@@ -30,9 +43,9 @@ try:
     logging.info("1.Drawing on the Horizontal image...")
     Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
-    draw.text((10, 0), 'hello world', font = font24, fill = 0)
-    draw.text((10, 20), '2.9inch e-Paper', font = font24, fill = 0)
-    draw.text((150, 0), u'微雪电子', font = font24, fill = 0)    
+    draw.text((10, 0), 'Hello Mushrooms', font = font24, fill = 0)
+  draw.text((10, 20), temperature, font=font24, fill=0)
+    draw.text((10, 40), humidity, font=font24, fill=0)
     draw.line((20, 50, 70, 100), fill = 0)
     draw.line((70, 50, 20, 100), fill = 0)
     draw.rectangle((20, 50, 70, 100), outline = 0)
@@ -42,36 +55,6 @@ try:
     draw.rectangle((80, 50, 130, 100), fill = 0)
     draw.chord((200, 50, 250, 100), 0, 360, fill = 0)
     epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
-    
-    # Drawing on the Vertical image
-    logging.info("2.Drawing on the Vertical image...")
-    Limage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(Limage)
-    draw.text((2, 0), 'hello world', font = font18, fill = 0)
-    draw.text((2, 20), '2.9inch epd', font = font18, fill = 0)
-    draw.text((20, 50), u'微雪电子', font = font18, fill = 0)
-    draw.line((10, 90, 60, 140), fill = 0)
-    draw.line((60, 90, 10, 140), fill = 0)
-    draw.rectangle((10, 90, 60, 140), outline = 0)
-    draw.line((95, 90, 95, 140), fill = 0)
-    draw.line((70, 115, 120, 115), fill = 0)
-    draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    draw.rectangle((10, 150, 60, 200), fill = 0)
-    draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(Limage))
-    time.sleep(2)
-    
-    logging.info("3.read bmp file")
-    Himage = Image.open(os.path.join(picdir, '2in9.bmp'))
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
-    
-    logging.info("4.read bmp file on window")
-    Himage2 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    Himage2.paste(bmp, (50,10))
-    epd.display(epd.getbuffer(Himage2))
     time.sleep(2)
     
 
