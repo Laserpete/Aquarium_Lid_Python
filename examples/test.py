f1628b52439339eb9fe7f5cd97ab2fd3921f7d94
LaserPete <pete.t.doyle@gmail.com>
2021-04-10 10:35:14 +0200

    Testing changes to display driver

--- examples/test.py
+++ examples/test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 from PIL import Image, ImageDraw, ImageFont
-from sensor import HTU21D
 import traceback
 import time
 from waveshare_epd import epd2in9_V2
@@ -16,44 +15,49 @@ if os.path.exists(libdir):
     sys.path.append(libdir)
 
 
-htu21d = HTU21D(1, 0x40)
-
-humid = htu21d.humidity()
-humid = round(humid.RH, 2)
-temp = htu21d.temperature()
-print(temp)
-C, F, K = temp
-C = round(C, 2)
-print(C)
-
-humidity = str(humid)
-temperature = str(C)
 logging.basicConfig(level=logging.DEBUG)
 
 try:
-    logging.info("epd4in2 Demo")
-
+    logging.info("epd2in9 V2 Demo")
     epd = epd2in9_V2.EPD()
+
     logging.info("init and Clear")
     epd.init()
-    epd.Clear()
+    epd.Clear(0xFF)
 
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    font35 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 35)
 
     # Drawing on the Horizontal image
     logging.info("1.Drawing on the Horizontal image...")
-    Himage = Image.new('1', (epd.width, epd.height),
+    Himage = Image.new('1', (epd.height, epd.width),
                        255)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
     draw.text((10, 0), 'Hello Mushrooms', font=font24, fill=0)
-    draw.text((10, 20), temperature, font=font24, fill=0)
-    draw.text((10, 40), humidity, font=font24, fill=0)
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
+    draw.text((10, 20), '2.9inch e-Paper', font=font24, fill=0)
 
-    epd.Clear()
+    # partial update
+    logging.info("5.show time")
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    time_draw = ImageDraw.Draw(time_image)
+    epd.display_Base(epd.getbuffer(time_image))
+    num = 0
+    while (True):
+        time_draw.rectangle((10, 10, 120, 50), fill=255)
+        time_draw.text((10, 10), time.strftime(
+            '%H:%M:%S'), font=font24, fill=0)
+        newimage = time_image.crop([10, 10, 120, 50])
+        time_image.paste(newimage, (10, 10))
+        epd.display_Partial(epd.getbuffer(time_image))
+
+        num = num + 1
+        if(num == 10):
+            break
+
+    logging.info("Clear...")
+    epd.init()
+    epd.Clear(0xFF)
+
     logging.info("Goto Sleep...")
     epd.sleep()
 
