3e8fac3daaa5f25effa7bc9b6fc9cc2d4d36067b
LaserPete <pete.t.doyle@gmail.com>
2021-04-10 11:11:12 +0200

    Fix stuff

--- examples/test.py
+++ examples/test.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+from PIL import Image, ImageDraw, ImageFont
+from sensor import HTU21D
+import traceback
+import time
+from waveshare_epd import epd2in9_V2
+import logging
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+picdir = os.path.join(os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
-import logging
-from waveshare_epd import epd2in9_V2
-import time
-from PIL import Image,ImageDraw,ImageFont
-import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
-from sensor import HTU21D
 
 htu21d = HTU21D(1, 0x40)
 
@@ -26,21 +28,21 @@ humidity = "Humidity : " + humidity + " %"
 
 temp = htu21d.temperature()
 C, F, K = temp
-C = round(C,2)
+C = round(C, 2)
 temperature = str(C)
 temperature = "Temperature : " + temperature + " C"
 
 try:
-    logging.info("epd2in9 V2 Demo") 
+    logging.info("epd2in9 V2 Demo")
     epd = epd2in9_V2.EPD()
 
     logging.info("init and Clear")
     epd.init()
     epd.Clear(0xFF)
-    
+
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    
+
     # # Drawing on the Horizontal image
     # logging.info("1.Drawing on the Horizontal image...")
     # Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
@@ -50,7 +52,6 @@ try:
     # draw.text((10, 40), humidity, font=font24, fill=0)
     # epd.display(epd.getbuffer(Himage))
     # time.sleep(2)
-    
 
     # partial update
     logging.info("5.show time")
@@ -59,30 +60,32 @@ try:
     epd.display_Base(epd.getbuffer(time_image))
     num = 0
     while (True):
-        time_draw.rectangle((10, 10, 120, 50), fill = 255)
-        draw.text((10, 0), 'Hello Mushrooms', font = font24, fill = 0)
-        draw.text((10, 20), temperature, font=font24, fill=0)
-        draw.text((10, 40), humidity, font=font24, fill=0)
-        time_draw.text((10, 60), time.strftime('%H:%M:%S'), font = font24, fill = 0)
+        draw = ImageDraw.Draw(Himage)
+        time_draw.rectangle((10, 10, 120, 50), fill=255)
+        time_draw((10, 0), 'Hello Mushrooms', font=font24, fill=0)
+        time_draw((10, 20), temperature, font=font24, fill=0)
+        time_draw((10, 40), humidity, font=font24, fill=0)
+        time_draw.text((10, 60), time.strftime(
+            '%H:%M:%S'), font=font24, fill=0)
         newimage = time_image.crop([10, 10, 120, 50])
-        time_image.paste(newimage, (10,10))  
+        time_image.paste(newimage, (10, 10))
         epd.display_Partial(epd.getbuffer(time_image))
-        
+
         num = num + 1
         if(num == 10):
             break
-            
+
     logging.info("Clear...")
     epd.init()
     epd.Clear(0xFF)
-    
+
     logging.info("Goto Sleep...")
     epd.sleep()
-    
+
 except IOError as e:
     logging.info(e)
-    
-except KeyboardInterrupt:    
+
+except KeyboardInterrupt:
     logging.info("ctrl + c:")
     epd2in9_V2.epdconfig.module_exit()
     exit()
