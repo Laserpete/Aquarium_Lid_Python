df201540b40c8769e95904ab0033241400181e2b
LaserPete <pete.t.doyle@gmail.com>
2021-04-11 08:11:19 +0200

    Try stuff

--- examples/test.py
+++ examples/test.py
@@ -51,14 +51,13 @@ try:
         temperature = "Temperature : " + temperature + " C"
         
         #time_draw.text((10, 80), 'Hello Mushrooms', font=font24, fill=0)
+        #time_draw.text((10, 40), temperature, font=font24, fill=0)
+        #time_draw.text((10, 60), humidity, font=font24, fill=0)
+        
         time_draw.rectangle((10, 10, 120, 50), fill=255)
-        
-        
         time_draw.text((10, 10), time.strftime('%H:%M:%S'), font=font24, fill=0)
-        time_draw.text((10, 40), temperature, font=font24, fill=0)
-        time_draw.text((10, 60), humidity, font=font24, fill=0)
-        
-        newimage = time_image.crop([10, 10, 120, 120])
+        time_draw.text((10, 30), time.strftime('%H:%M:%S'), font=font24, fill=0)
+        newimage = time_image.crop([10, 10, 120, 50])
         time_image.paste(newimage, (10, 10))
         epd.display_Partial(epd.getbuffer(time_image))
 
