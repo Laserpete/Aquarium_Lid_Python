0634d9d75aaacb1887fa3da9a1af12a1f95d643c
LaserPete <pete.t.doyle@gmail.com>
2021-04-12 16:45:29 +0200

    Change lib path

--- Aquarium_Lid_Python.py
+++ Aquarium_Lid_Python.py
@@ -3,7 +3,7 @@
 import sys
 import os
 picdir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'lib')
+libdir = /home/pi/Aquarium_Lid_Python/lib
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
